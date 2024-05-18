8f9baccc3748e60e50610cf3f62ee45a65efa528
merlab-inovasyon <f211213069@ktun.edu.tr>
2024-05-18 21:20:03 +0300

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	lib/anasayfalar/oyunPage.dart

--- lib/anasayfalar/Game2.dart
+++ lib/anasayfalar/Game2.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'dart:math';
 
 class Game2 extends StatefulWidget {
   @override
@@ -7,39 +11,159 @@ class Game2 extends StatefulWidget {
 
 class _Game2State extends State<Game2> {
   String selectedOption = '';
+  String videoUrl = '';
+  List<String> options = [];
+  bool isLoading = false;
+  int correctAnswers = 0;
+  int questionCount = 0;
+  String correctOption = '';
+  VideoPlayerController? _controller;
 
-  void selectOption(String option) {
+  @override
+  void initState() {
+    super.initState();
+    fetchGameData();
+  }
+
+  Future<void> fetchGameData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      // Firestore'dan tüm soruları çek
+      QuerySnapshot snapshot = await FirebaseFirestore.instance
+          .collection('games2')
+          .get();
+
+      // Tüm belgeleri listeye dönüştür
+      List<QueryDocumentSnapshot> documents = snapshot.docs;
+
+      if (documents.isNotEmpty) {
+        // Rastgele bir belge seç
+        Random random = Random();
+        int randomIndex = random.nextInt(documents.length);
+        DocumentSnapshot selectedDoc = documents[randomIndex];
+
+        // Rastgele seçilen belgeden verileri al
+        Map<String, dynamic>? data = selectedDoc.data() as Map<String, dynamic>?;
+
+        if (data != null) {
+          // Belgedeki tüm "question" anahtarlarını alın
+          List<String> questionKeys = data.keys.where((key) => key.startsWith('question')).toList();
+
+          if (questionKeys.isNotEmpty) {
+            // Rastgele bir "question" anahtarı seç
+            String randomQuestionKey = questionKeys[random.nextInt(questionKeys.length)];
+            List<dynamic> questionData = data[randomQuestionKey];
+            if (questionData.length >= 5) {
+              options = List<String>.from(questionData.sublist(0, 4)); // İlk 4 eleman seçenekler
+              correctOption = questionData[0]; // Doğru seçenek
+              String videoPath = questionData[4]; // 5. eleman video yolu
+
+              // Firebase Storage'dan indirme URL'sini al
+              String downloadUrl = await FirebaseStorage.instance
+                  .ref(videoPath)
+                  .getDownloadURL();
+
+              setState(() {
+                videoUrl = downloadUrl;
+                selectedOption = '';
+                options.shuffle(); // Seçenekleri karıştır
+                _controller?.dispose();
+                _controller = VideoPlayerController.network(videoUrl)
+                  ..initialize().then((_) {
+                    setState(() {});
+                    _controller!.play();
+                  });
+              });
+            } else {
+              print('Error: Question data does not have enough elements.');
+            }
+          } else {
+            print('Error: No question fields found in the document.');
+          }
+        } else {
+          print('Error: Document data is null.');
+        }
+      } else {
+        print('Error: No documents found in the collection.');
+      }
+    } catch (e) {
+      print('Error fetching game data: $e');
+    } finally {
+      setState(() {
+        isLoading = false;
+        questionCount++;
+      });
+    }
+  }
+
+  void onOptionSelected(String option) {
     setState(() {
       selectedOption = option;
     });
 
-    if (option == 'Cheese') {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Doğru!'),
-          backgroundColor: Colors.green,
-        ),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Yanlış!'),
-          backgroundColor: Colors.red,
-        ),
-      );
+    if (option == correctOption) {
+      correctAnswers++;
+      print('Correct answers: $correctAnswers');
     }
-  }//
+
+    // Cevap verildikten sonra bir süre bekleyip yeni soru yükle
+    Future.delayed(Duration(seconds: 2), () {
+      if (questionCount < 5) {
+        fetchGameData();
+      } else {
+        showGameOverDialog();
+      }
+    });
+  }
+
+  void showGameOverDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Game Over"),
+          content: Text("Total correct answers: $correctAnswers"),
+          actions: [
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                exitGame();
+              },
+            ),
+          ],
+        );
+      },
+      barrierDismissible: false, // Çarpı butonunu kapatır
+    );
+  }
+
+  void exitGame() {
+    Navigator.of(context).pop(); // Dialogu kapat
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.deepOrange,
       appBar: AppBar(
-        title: Text('Game 2'),
+        title: Text('Video Oyunu',style: TextStyle(fontWeight: FontWeight.bold),),
         backgroundColor: Colors.deepOrange,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
+        child: isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Expanded(
@@ -52,16 +176,18 @@ class _Game2State extends State<Game2> {
                       color: Colors.grey.withOpacity(0.5),
                       spreadRadius: 5,
                       blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
+                      offset: Offset(0, 3),
                     ),
                   ],
                 ),
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(20.0),
-                  child: Image.asset(
-                    'assets/cheese.png', // Peynir resmini buraya ekle
-                    fit: BoxFit.cover,
-                  ),
+                  child: _controller != null && _controller!.value.isInitialized
+                      ? AspectRatio(
+                    aspectRatio: _controller!.value.aspectRatio,
+                    child: VideoPlayer(_controller!),
+                  )
+                      : Center(child: CircularProgressIndicator()),
                 ),
               ),
             ),
@@ -76,26 +202,11 @@ class _Game2State extends State<Game2> {
               ),
             ),
             SizedBox(height: 20),
-            OptionButton(
-              option: 'Cheese',
-              isSelected: selectedOption == 'Cheese',
-              onTap: () => selectOption('Cheese'),
-            ),
-            OptionButton(
-              option: 'Bread',
-              isSelected: selectedOption == 'Bread',
-              onTap: () => selectOption('Bread'),
-            ),
-            OptionButton(
-              option: 'Apple',
-              isSelected: selectedOption == 'Apple',
-              onTap: () => selectOption('Apple'),
-            ),
-            OptionButton(
-              option: 'Milk',
-              isSelected: selectedOption == 'Milk',
-              onTap: () => selectOption('Milk'),
-            ),
+            ...options.map((option) => OptionButton(
+              option: option,
+              isSelected: selectedOption == option,
+              onTap: () => onOptionSelected(option),
+            )).toList(),
           ],
         ),
       ),
--- lib/anasayfalar/Game3.dart
+++ lib/anasayfalar/Game3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Game3 extends StatefulWidget {
   const Game3({super.key});
 
@@ -7,8 +8,133 @@ class Game3 extends StatefulWidget {
 }
 
 class _Game3State extends State<Game3> {
+  String selectedOption = '';
+
+  void selectOption(String option) {
+    setState(() {
+      selectedOption = option;
+    });
+
+    // Cevap kontrolü ve kullanıcıya bildirim
+    if (option == 'CorrectOption') {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Doğru!'),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Yanlış!'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();//
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      appBar: AppBar(
+        title: Text('Kelime Oyunu',style: TextStyle(fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.asset(
+                        'resimler/bulut.jpeg', // Fotoğrafı buraya ekleyin
+                        fit: BoxFit.fill  ,
+                      ),
+                      Container(
+                        alignment: Alignment(0.0, -0.5), // Yazıyı üst-orta hizalar
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            'Bu cümleyi buraya yazın', // Cümle buraya gelecek
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ...['Option 1', 'Option 2', 'Option 3', 'CorrectOption'].map((option) => OptionButton(
+              option: option,
+              isSelected: selectedOption == option,
+              onTap: () => selectOption(option),
+            )).toList(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OptionButton extends StatelessWidget {
+  final String option;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  OptionButton({
+    required this.option,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.deepOrange[100] : Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: isSelected ? Colors.deepOrange : Colors.grey,
+              width: 2,
+            ),
+          ),
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                option,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: isSelected ? Colors.deepOrange : Colors.black,
+                  fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
